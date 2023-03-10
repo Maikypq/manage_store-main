2d2c2f96a2ea08b54149e04b4167a93d2d91524e
Maikypq <126524781+Maikypq@users.noreply.github.com>
2023-03-09 22:05:49 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/manage_store/model/Seller.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.manage_store.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Seller {
+    private String code;
+    private String name;
+}
